70e6fe0fc17927c17415b72d0a280653e2997950
Hiep <nguyenminhhiep@kng.vn>
2017-10-19 17:54:34 +0700

    Firt comit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 // remember to use the Post model
 use App\Post;
+// remember to use
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
@@ -18,5 +20,31 @@ class PostsController extends Controller
         $posts = Post::paginate(5);
 
         return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Favorite a particular post
+     *
+     * @param  Post $post
+     * @return Response
+     */
+    public function favoritePost(Post $post)
+    {
+        Auth::user()->favorites()->attach($post->id);
+
+        return back();
+    }
+
+    /**
+     * Unfavorite a particular post
+     *
+     * @param  Post $post
+     * @return Response
+     */
+    public function unFavoritePost(Post $post)
+    {
+        Auth::user()->favorites()->detach($post->id);
+
+        return back();
     }
 }
